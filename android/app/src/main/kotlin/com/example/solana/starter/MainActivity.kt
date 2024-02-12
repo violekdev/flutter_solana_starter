50b7b2aafbb9c1446e04bcdb8c79b0e894bd2918
violekdev <71582866+violekdev@users.noreply.github.com>
2024-02-12 15:57:18 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/solana/starter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.solana.starter.flutter_solana_starter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_solana_starter/app/app.dart';
+import 'package:flutter_solana_starter/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
